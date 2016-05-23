18906e3149b95026fe64d0e8192edc95cf5b350c
jonathandavidblack <jonnybl915@gmail.com>
2016-05-23 11:26:35 -0400

    handle exceptions gracefully

---
+++ src/com/theironyard/jdblack/Main.java
@@ -0,0 +1,63 @@
+package com.theironyard.jdblack;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<ToDoItem> items = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Create new Item");
+            System.out.println("2. Toggle Item");
+            System.out.println("3. List Items");
+
+            String option = scanner.nextLine();
+
+            switch (option){
+                case "1":
+                    //create new item
+                    System.out.println("Enter your to do item");
+                    String text = scanner.nextLine();
+                    ToDoItem item = new ToDoItem(text, false);
+                    items.add(item); // adds it to the array list
+                    break;
+
+                case "2":
+                  //toggle item
+                    System.out.println("Enter the # of the item you wish to toggle:");
+                    String numStr = scanner.nextLine();
+                    try {
+                        int num = Integer.valueOf(numStr);
+                        ToDoItem tempItem = items.get(num - 1);
+                        tempItem.isDone = !tempItem.isDone; //nice way of flipping current value
+                    }
+                    catch (NumberFormatException e){
+                        System.out.println("Error: You Did Not Type A Number!"); // nice way to handle exceptions without crashing the program
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Error: Number Is Not Valid");
+                    }
+                    break;
+
+                case "3":
+                    //list item
+                    int i = 1;
+                    for (ToDoItem toDoItem : items){
+                        String checkbox = "[ ]";
+                        if (toDoItem.isDone){
+                            checkbox = "[x]";
+                        }
+                        System.out.println(checkbox + " " + i + ". " + toDoItem.text);
+                        i++; //incrementing the numbers beside the items
+                    }
+                    break;
+
+                default: //this is the "else" portion of the switch loop
+                    System.out.println("Invalid option.");
+            }
+        }
+    }
+}
